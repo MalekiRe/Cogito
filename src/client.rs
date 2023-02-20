e5853fcd6c24903ecbc827dd42eab4d810449efb
MalekiRe <pocmalek@gmail.com>
2023-02-20 13:08:16 -0800

    make audio better?

--- src/client.rs
+++ src/client.rs
@@ -41,14 +41,17 @@ pub fn client() -> Result<()> {
 
     sk.run(|sk| {
         locomotion_tracker.locomotion_update(sk);
-        let len = sound.unread_samples();
-        //println!("len: {}", len);
-        let mut samples_with_pos = SamplesWithPos::new(sk.input_head().position.into(), client.remote_address(), len);
-        sound.read_samples(samples_with_pos.samples.as_mut_slice());
-        let bytes = bincode::serialize(&samples_with_pos).unwrap();
-        //println!("sending bytes: {:#?}", bytes);
-        client.send(bytes.into_boxed_slice(), 0, SendMode::Persistent);
-
+        // sample_counter += 1;
+        // if sample_counter >= 60 {
+        //     sample_counter = 0;
+            let len = sound.unread_samples();
+            //println!("len: {}", len);
+            let mut samples_with_pos = SamplesWithPos::new(sk.input_head().position.into(), client.remote_address(), len);
+            sound.read_samples(samples_with_pos.samples.as_mut_slice());
+            let bytes = bincode::serialize(&samples_with_pos).unwrap();
+            //println!("sending bytes: {:#?}", bytes);
+            client.send(bytes.into_boxed_slice(), 0, SendMode::Persistent);
+        //}
         for event in client.step() {
             match event {
                 uflow::client::Event::Connect => {
