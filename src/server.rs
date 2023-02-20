e6a0a9a37be5b60158ea6d2e69d39ba752a3a833
MalekiRe <pocmalek@gmail.com>
2023-02-20 14:24:40 -0800

    make audio better?

--- src/server.rs
+++ src/server.rs
@@ -50,6 +50,7 @@ pub fn server() -> Result<()> {
         // Flush outbound UDP frames
         //server.flush();
         //std::thread::sleep(Duration::from_nanos(2));
+        server.flush();
         std::thread::sleep(std::time::Duration::from_millis(300));
         //std::thread::sleep(std::time::Duration::from_millis(30));
     }
